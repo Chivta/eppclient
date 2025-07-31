f9473aceedb93a469fb1565bd1013e2b3c4cb278
Chivta <arvlas78@gmail.com>
2025-07-31 15:53:15 +0300

    Added wrap_in_epp_element function to replace repetitive code.

--- XMLGenerator.py
+++ XMLGenerator.py
@@ -1,8 +1,15 @@
 # XML templates for requests
+from config import ns
+
+def wrap_in_epp_element(content: str) -> str:
+    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+ <epp xmlns="{ns["epp"]}">
+    {content}
+ </epp>'''
+
 
 def login(cl_id: str, password: str) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    return wrap_in_epp_element(f'''
   <command>
     <login>
       <clID>{cl_id}</clID>
@@ -24,35 +31,28 @@ def login(cl_id: str, password: str) -> str:
         </svcExtension>
       </svcs>
     </login>
-  </command>
-</epp>'''
+  </command>''')
 
 
 def logout() -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    return wrap_in_epp_element(f'''
   <command>
     <logout/>
-  </command>
-</epp>'''
+  </command>''')
 
 def domain_check(domains: list[str]) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
- <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
-   <command>
+    return wrap_in_epp_element(f'''
+    <command>
      <check>
        <domain:check xmlns:domain="http://hostmaster.ua/epp/domain-1.1">
          {"".join(f"<domain:name>{domain}</domain:name>" for domain in domains)}
        </domain:check>
      </check>
-   </command>
- </epp>'''
+   </command>''')
 
 def hello() -> str:
-    return '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
- <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
-   <hello/>
- </epp>'''
+    return wrap_in_epp_element('''
+   <hello/>''')
 
 def build_hosts(ns : list) -> str:
     if not ns: return ""
@@ -73,20 +73,17 @@ def build_hosts(ns : list) -> str:
     return result
 
 def domain_info(domain: str) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
-      <command>
+    return wrap_in_epp_element(f'''
+    <command>
         <info>
           <domain:info xmlns:domain="http://hostmaster.ua/epp/domain-1.1">
             <domain:name>{domain}</domain:name>
           </domain:info>
         </info>
-      </command>
-    </epp>'''
+    </command>''')
 
 def domain_create(name : str, period : int, ns, registrant : str, contacts : list[tuple[str, str]]) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-  <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    return wrap_in_epp_element(f'''
     <command>
       <create>
         <domain:create xmlns:domain="http://hostmaster.ua/epp/domain-1.1">
@@ -97,40 +94,32 @@ def domain_create(name : str, period : int, ns, registrant : str, contacts : lis
           {"\n".join(f'<domain:contact type="{type_}">{text}</domain:contact>' for type_, text in contacts)}
         </domain:create>
       </create>
-    </command>
-  </epp>'''
+    </command>''')
 
 def host_check(hosts) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
- <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    return wrap_in_epp_element(f'''
    <command>
      <check>
        <host:check xmlns:host="http://hostmaster.ua/epp/host-1.1">
          {"\n".join(f"<host:name>{host}</host:name>" for host in hosts) }
        </host:check>
      </check>
-   </command>
-</epp>
-'''
+   </command>''')
 
 def contact_info(contact) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
- <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    return wrap_in_epp_element(f'''
    <command>
      <info>
        <contact:info xmlns:contact="http://hostmaster.ua/epp/contact-1.1">
          <contact:id>{contact}</contact:id>
        </contact:info>
      </info>
-   </command>
- </epp>
-'''
+   </command>''')
 
 
 def host_create(name,ipv4,ipv6) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
- <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
-   <command>
+    return wrap_in_epp_element(f'''
+    <command>
      <create>
        <host:create xmlns:host="http://hostmaster.ua/epp/host-1.1">
          <host:name>{name}</host:name>
@@ -138,70 +127,68 @@ def host_create(name,ipv4,ipv6) -> str:
            {f"<host:addr ip='v6'>{ipv6}</host:addr>" if ipv6 else ""}
        </host:create>
      </create>
-   </command>
- </epp>'''
+   </command>''')
 
 
 def contact_create(contact_id, name, city, country_code, email, password) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
-  <command>
-    <create>
-      <contact:create xmlns:contact="http://hostmaster.ua/epp/contact-1.1">
-        <contact:id>{contact_id}</contact:id>
-        <contact:postalInfo type="int">
-          <contact:name>{name}</contact:name>
-          <contact:addr>
-            <contact:city>{city}</contact:city>
-            <contact:cc>{country_code}</contact:cc>
-          </contact:addr>
-        </contact:postalInfo>
-        <contact:email>{email}</contact:email>
-        <contact:authInfo>
-          <contact:pw>{password}</contact:pw>
-        </contact:authInfo>
-      </contact:create>
-    </create>
-  </command>
-</epp>'''
+    return wrap_in_epp_element(f'''
+    <command>
+        <create>
+          <contact:create xmlns:contact="http://hostmaster.ua/epp/contact-1.1">
+            <contact:id>{contact_id}</contact:id>
+            <contact:postalInfo type="int">
+              <contact:name>{name}</contact:name>
+              <contact:addr>
+                <contact:city>{city}</contact:city>
+                <contact:cc>{country_code}</contact:cc>
+              </contact:addr>
+            </contact:postalInfo>
+            <contact:email>{email}</contact:email>
+            <contact:authInfo>
+              <contact:pw>{password}</contact:pw>
+            </contact:authInfo>
+          </contact:create>
+        </create>
+    </command>''')
 
 def domain_delete(domain:str) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-  <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    return wrap_in_epp_element(f'''
     <command>
       <delete>
         <domain:delete xmlns:domain="http://hostmaster.ua/epp/domain-1.1">
           <domain:name>{domain}</domain:name>
         </domain:delete>
       </delete>
-    </command>
-  </epp>
-    '''
+    </command>''')
 
 
 def contact_delete(contact_id:str) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-  <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+    return wrap_in_epp_element(f'''
     <command>
       <delete>
         <contact:delete xmlns:contact="http://hostmaster.ua/epp/contact-1.1">
           <contact:id>{contact_id}</contact:id>
         </contact:delete>
       </delete>
-    </command>
-  </epp>
-'''
+    </command>''')
 
+def contact_check(contact_id:str) -> str:
+    return wrap_in_epp_element(f'''
+    <command>
+     <check>
+       <contact:check
+        xmlns:contact="http://hostmaster.ua/epp/contact-1.1">
+         <contact:id>{contact_id}</contact:id>
+       </contact:check>
+     </check>
+   </command>''')
 
 def host_info(name) -> str:
-    return f'''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
- <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
-   <command>
+    return wrap_in_epp_element(f'''
+    <command>
      <info>
        <host:info xmlns:host="http://hostmaster.ua/epp/host-1.1">
          <host:name>{name}</host:name>
        </host:info>
      </info>
-   </command>
- </epp>
-'''
+   </command>''')
