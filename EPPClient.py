0cf091b591f3fed37deb63da89f1ae04a85ed9a5
artemvlasenko <arvlas78@gmail.com>
2025-08-04 14:23:45 +0300

    Minor changes to naming in EPPClient.py

--- EPPClient.py
+++ EPPClient.py
@@ -12,55 +12,54 @@ class EPPClient:
         return response
 
     def login(self, cl_id, password):
-        login_xml = XMLGenerator.login(cl_id, password)
-        response = self.stream.exchange_messages(login_xml)
-
+        request = XMLGenerator.login(cl_id, password)
+        response = self.stream.exchange_messages(request)
         return response
 
     def logout(self):
-        logout_xml = XMLGenerator.logout()
-        response = self.stream.exchange_messages(logout_xml)
+        request = XMLGenerator.logout()
+        response = self.stream.exchange_messages(request)
         return response
 
     def hello(self):
-        hello_xml = XMLGenerator.hello()
-        response = self.stream.exchange_messages(hello_xml)
+        request = XMLGenerator.hello()
+        response = self.stream.exchange_messages(request)
         return response
 
     def domain_check(self, domains: list[str]):
-        check_domain_xml = XMLGenerator.domain_check(domains)
-        response = self.stream.exchange_messages(check_domain_xml)
+        request = XMLGenerator.domain_check(domains)
+        response = self.stream.exchange_messages(request)
         return response
 
     def domain_create(self, name: str, period: int, ns: list[str | tuple[str, dict[str:str]]], registrant: str,
-                      contacts: list[(str, str)]):
-        domain_create_xml = XMLGenerator.domain_create(name, period, ns, registrant, contacts)
-        response = self.stream.exchange_messages(domain_create_xml)
+                      contacts: list[tuple[str, str]]):
+        request = XMLGenerator.domain_create(name, period, ns, registrant, contacts)
+        response = self.stream.exchange_messages(request)
         return response
 
     def domain_info(self, domain: str):
-        domain_info_xml = XMLGenerator.domain_info(domain)
-        response = self.stream.exchange_messages(domain_info_xml)
+        request = XMLGenerator.domain_info(domain)
+        response = self.stream.exchange_messages(request)
         return response
 
     def domain_delete(self, domain: str):
-        domain_delete_xml = XMLGenerator.domain_delete(domain)
-        response = self.stream.exchange_messages(domain_delete_xml)
+        request = XMLGenerator.domain_delete(domain)
+        response = self.stream.exchange_messages(request)
         return response
 
     def host_check(self, hosts):
-        host_check_xml = XMLGenerator.host_check(hosts)
-        response = self.stream.exchange_messages(host_check_xml)
+        request = XMLGenerator.host_check(hosts)
+        response = self.stream.exchange_messages(request)
         return response
 
     def host_create(self, name: str, ipv4="", ipv6=""):
-        host_create_xml = XMLGenerator.host_create(name, ipv4, ipv6)
-        response = self.stream.exchange_messages(host_create_xml)
+        request = XMLGenerator.host_create(name, ipv4, ipv6)
+        response = self.stream.exchange_messages(request)
         return response
 
     def host_info(self, name):
-        host_info_xml = XMLGenerator.host_info(name)
-        response = self.stream.exchange_messages(host_info_xml)
+        request = XMLGenerator.host_info(name)
+        response = self.stream.exchange_messages(request)
         return response
 
     def host_delete(self, name):
@@ -69,28 +68,22 @@ class EPPClient:
         return response
 
     def contact_info(self, contacts):
-        contact_info_xml = XMLGenerator.contact_info(contacts)
-
-        response = self.stream.exchange_messages(contact_info_xml)
+        request = XMLGenerator.contact_info(contacts)
+        response = self.stream.exchange_messages(request)
         return response
 
     def contact_create(self, contact_id, name="test", city="test", country_code="UA", email="test@test.test",
                        password="test"):
-        contact_create_xml = XMLGenerator.contact_create(contact_id, name, city, country_code, email, password)
-
-        response = self.stream.exchange_messages(contact_create_xml)
+        request = XMLGenerator.contact_create(contact_id, name, city, country_code, email, password)
+        response = self.stream.exchange_messages(request)
         return response
 
     def contact_delete(self, contact_id):
-        contact_delete_xml = XMLGenerator.contact_delete(contact_id)
-
-        response = self.stream.exchange_messages(contact_delete_xml)
-
+        request = XMLGenerator.contact_delete(contact_id)
+        response = self.stream.exchange_messages(request)
         return response
 
     def contact_check(self, contact_id):
-        contact_check_xml = XMLGenerator.contact_check(contact_id)
-
-        response = self.stream.exchange_messages(contact_check_xml)
-
+        request = XMLGenerator.contact_check(contact_id)
+        response = self.stream.exchange_messages(request)
         return response
